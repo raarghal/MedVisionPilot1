04b7c21b8a87fbb24ddb9bb48ab20dc0fc8c65b4
raarghal <raarghal@umich.edu>
2020-06-24 00:39:14 -0400

    corrected serial port for use with RPi

--- pilot_proto.py
+++ pilot_proto.py
@@ -5,7 +5,7 @@ import serial
 import subprocess as cmd
 import os
 
-ser = serial.Serial(port = '/dev/tty.usbserial-1430', baudrate = 9600, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS, timeout = 1)
+ser = serial.Serial(port = '/dev/ttyUSB0', baudrate = 9600, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS, timeout = 1)
 iter = 0
 
 masterlog = open('master.csv', 'w')
@@ -28,6 +28,6 @@ while 1:
         time.sleep(1)
 
     if (iter%300 == 0):
-        #cp = cmd.run("git add .", check=True, shell=True)
-        #cp = cmd.run("git commit -m " + time.ctime(), check=True, shell=True)
-        #cp = cmd.run("git push -u origin master -f", check=True, shell=True)
+        cp = cmd.run("git add .", check=True, shell=True)
+        cp = cmd.run("git commit -m " + time.ctime(), check=True, shell=True)
+        cp = cmd.run("git push -u origin master", check=True, shell=True)
